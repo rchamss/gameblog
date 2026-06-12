1217f764fe06b2b20d83a51e0801f7273cc865ad
SuleimanAl-Qahtani <suleiman.majedabd@gmail.com>
2026-06-12 17:15:46 +0000

    Atualiza dados complementares dos jogos

--- src/data/complementaryData.js
+++ src/data/complementaryData.js
@@ -1,3 +1,280 @@
 const jogosData = [
-    {nome: "Red Dead Redemption 2"}
-]
+    {
+        nome: "The Witcher 3: Wild Hunt",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/coaarl.webp",
+        video: "https://www.youtube.com/embed/MqQ3LeBgGDM?si=oV24BDMbAIvaY2aB",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/mnljdjtrh44x4snmierh.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sciabd.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/z5t0yuhyiiui1ickwhgj.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/em1y2ugcwy2myuhvb9db.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/usxccsncekxg0wd1v6ee.webp"
+        ]
+    },
+    {
+        nome: "Red Dead Redemption 2",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1q1f.webp",
+        video: "https://www.youtube.com/embed/94B-38sX5fs?si=yheWnmDHPTmNhfLv",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/xegpfnsvlyeld0zkjnrc.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/c9xalka7stjkx4mes7kp.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/dhw6ucx9laj5esv6rngn.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/h8f9uojkzvaau8pxsyxi.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/kcfpf8wa8esalk0qkpo5.webp"
+        ]
+    },
+    {
+        nome: "The Legend of Zelda: Breath of the Wild",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co3p2d.webp",
+        video: "https://www.youtube.com/embed/zw47_q9wbBE?si=Il_g-gRcrsPkEP2R",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/sckj69.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sckj6a.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sckj6b.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sckj6c.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sckj6d.webp"
+        ]
+    },
+    {
+        nome: "Minecraft",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co8fu7.webp",
+        video: "https://www.youtube.com/embed/X-fMtNOS_gU?si=3cCqo0ejrDR41SdR",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar4nm5.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar4qpp.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar4nm4.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc10jch.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc8d2o.webp"
+        ]
+    },
+    {
+        nome: "Stardew Valley",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/coa93h.webp",
+        video: "https://www.youtube.com/embed/8A7A1X1TVNc?si=w25ePwEVSpYSOUpc",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/rhxs1x9w5hf5kde2osf5.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/iwswpvxa9ytrpk8yjcyx.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sw7rtba7p1xs77klsime.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ak7zmag7zztpqha1jjde.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/g1aakqbkp2quq0krqeky.webp"
+        ]
+    },
+    {
+        nome: "Portal 2",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1rs4.webp",
+        video: "https://www.youtube.com/embed/SJ5JY-ll2HY?si=o3SQyCYzVK-wBlss",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/rvrge8js7xnhr4z1vrbk.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/m4hdhjptmmwvrjbojzp0.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/vghkwbdabx0cri7thb9y.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/cmxaa4r52exlqvzwtxkh.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/i9ys3zdhph1mh3futdit.webp"
+        ]
+    },
+    {
+        nome: "Half-Life: Alyx",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co87vg.webp",
+        video: "https://www.youtube.com/embed/O2W0N3uKXmo?si=HoWtBrMb4gv25cyb",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar6d8.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc7dah.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc7dag.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc7dae.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc7dad.webp"
+        ]
+    },
+    {
+        nome: "Cyberpunk 2077",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/coaih8.webp",
+        video: "https://www.youtube.com/embed/qIcTM8WXFjk?si=Q890vxCbLdr1N-5H",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/lelfskpwy4slftl3qdeb.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ts8wtae3t6aghttvtt2s.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar3m0i.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar3m0k.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar3m0j.webp"
+        ]
+    },
+    {
+        nome: "Among Us",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co6kqt.webp",
+        video: "https://www.youtube.com/embed/JpalSQkmp2g?si=TKEkOzL5M_vZ_QOA",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar7tq.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sceeq3.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sceeq2.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sceeq4.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sceeq6.webp"
+        ]
+    },
+    {
+        nome: "A Lenda do Herói",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2k79.webp",
+        video: "https://www.youtube.com/embed/4VgRzOu-VU0?si=Rp28dHZdYgXRvi2V",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc8qhy.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc8qhv.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc8qi0.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc8qhz.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc8qhx.webp"
+        ]
+    },
+    {
+        nome: "Enigma do Medo",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co93ft.webp",
+        video: "https://www.youtube.com/embed/OvibI1wQZAA?si=M9SOg3njPQiMatwG",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/schjpr.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/schjpq.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/schjpp.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/schjpo.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/schjpt.webp"
+        ]
+    },
+    {
+        nome: "Horizon Zero Dawn",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co90ti.webp",
+        video: "https://www.youtube.com/embed/rgDYyxOKBgg?si=qvCU8QyJfUYkfGRm",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/scuchq.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/scuchk.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/scuchl.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/scuchm.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/scuchp.webp"
+        ]
+    },
+    {
+        nome: "Bloodborne",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/cob99l.webp",
+        video: "https://www.youtube.com/embed/AXiUYQ8eunw?si=ribzi2eG_AMue_7a",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar3mkv.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/uqmif3sshdrbbcd0pu8l.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/adssvwsfbaxcrucjolv9.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/hc9nb80b0xni5wxm5zh3.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/aokcsspzrqyaaoug7jnn.webp"
+        ]
+    },
+    {
+        nome: "Call of Duty: Modern Warfare",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1rsg.webp",
+        video: "https://www.youtube.com/embed/bH1lHCirCGI?si=7EmeYIcchNGKs8j0",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc87l5.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc87l7.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc87l6.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc87l8.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6qem.webp"
+        ]
+    },
+    {
+        nome: "Sekiro: Shadows Die Twice",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2a23.webp",
+        video: "https://www.youtube.com/embed/J3AbM_JRkWQ?si=hR4ho3YBwdR08nDQ",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/smay0rol1vbibwop2t7n.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/cmootzhyx1l7scxeihej.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sn1rmqcvrwomyuttaoyq.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ocxemodvr6oa72argpyo.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/jjn6e6ivua5u142iukql.webp"
+        ]
+    },
+    {
+        nome: "The Elder Scrolls V: Skyrim",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/cobt0i.webp",
+        video: "https://www.youtube.com/embed/0mHGygvlKCQ?si=k0_cHiYUNfoOB5v5",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/xzk2h41fiye7uwbhc6ub.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/x5bbaqvgbpaz4hzlfeqb.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/t0mus35qrgclafo1ql8k.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/muv70yw3rds1cw8ymr5v.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sctdy5.webp"
+        ]
+    },
+    {
+        nome: "Fallout 4",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1yc6.webp",
+        video: "https://www.youtube.com/embed/X5aJfebzkrM?si=cSusIp6n24fRHLWr",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/whhptvhci1bdoqolofjo.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/acrxfc2grr69wfbql8ax.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ufocunt4ze1rjomybg2h.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/twgwp1xobnae4kbky2hw.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sctl85.webp"
+        ]
+    },
+    {
+        nome: "Monster Hunter: World",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1rst.webp",
+        video: "https://www.youtube.com/embed/TjKQ3fbB27Y?si=z4ho7X2-_fVO_n20",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/u3y9wqhlh2ufby0sb6yt.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/lyczmwkawvhi7xvolzlv.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/ojdwmo8mhipudkalaxlo.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/oydgjnzydmdfwd3vtmp9.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/c9rskkieuhapebstfxys.webp"
+        ]
+    },
+    {
+        nome: "Persona 5 Royal",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co8ozn.webp",
+        video: "https://www.youtube.com/embed/IR_7H_zzU7Y?si=jFjhMdErX6MBLgQH",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/ar3u1t.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6vq2.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6vps.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6vpy.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6vq0.webp"
+        ]
+    },
+    {
+        nome: "Yakuza: Like a Dragon",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2em9.webp",
+        video: "https://www.youtube.com/embed/_uBK1MMC5KQ?si=4ek3QsUKi1uIJeLU",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6wyn.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6wyl.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6wyo.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6wym.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/sc6wyp.webp"
+        ]
+    },
+    {
+        nome: "Resident Evil 7: Biohazard",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/cobd4o.webp",
+        video: "https://www.youtube.com/embed/0osKz2rOTHo?si=hXbkBxW58bwrv7bS",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/kvo8i8r4frk2hlvsyebm.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/io6hffcfxfgtibwwn9jl.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/wyn4sipweizrkjvshid9.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/tc63ssff6rzvaiq1xjuw.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/r03xsdmmrxyddimojni1.webp"
+        ]
+    },
+    {
+        nome: "Grand Theft Auto V",
+        capa: "https://images.igdb.com/igdb/image/upload/t_cover_big/co2lbd.webp",
+        video: "https://www.youtube.com/embed/PIF_fqFZEuk?si=ZsV81kA9pEl_xXs2",
+        imagens: [
+            "https://images.igdb.com/igdb/image/upload/t_720p/s7eojwfwa0viamaa68gs.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/lg34eavl7phmabgceods.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/btyih5vofmjwex2tkm8w.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/gtng284ejjted82pncis.webp",
+            "https://images.igdb.com/igdb/image/upload/t_720p/rrrqdvbz5lhuhqudjgza.webp"
+        ]
+    }
+];
+
+
+const jogoPlaceholder = {
+    nome: "Jogo Personalizado",
+    capa: "https://placehold.net/800x600.png",
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    imagens: [
+        "https://placehold.net/800x600.png",
+        "https://placehold.net/800x600.png",
+        "https://placehold.net/800x600.png",
+        "https://placehold.net/800x600.png",
+        "https://placehold.net/800x600.png"
+    ]
+};
